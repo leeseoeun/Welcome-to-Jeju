2d090a99d752fb9aab460705ea0f889807d7af54
leeseoeun <seoeun.shuen@gmail.com>
2021-12-13 03:39:06 +0900

    1212 PlacePhotoDao 수정: findAllByPlaceId 추가

--- app-server/src/main/java/com/welcomeToJeju/wtj/dao/PlaceCommentDao.java
+++ app-server/src/main/java/com/welcomeToJeju/wtj/dao/PlaceCommentDao.java
@@ -1,10 +1,15 @@
 package com.welcomeToJeju.wtj.dao;
 
 import java.util.HashMap;
+import java.util.List;
+import com.welcomeToJeju.wtj.domain.PlaceComment;
 
 public interface PlaceCommentDao {
 
   void insert(HashMap<String,Object> param) throws Exception;
   void delete(String id) throws Exception;
+
+  List<PlaceComment> findAllByPlaceId(String id) throws Exception;
+
 
 }
--- app-server/src/main/java/com/welcomeToJeju/wtj/dao/PlacePhotoDao.java
+++ app-server/src/main/java/com/welcomeToJeju/wtj/dao/PlacePhotoDao.java
@@ -1,10 +1,15 @@
 package com.welcomeToJeju.wtj.dao;
 
 import java.util.HashMap;
+import java.util.List;
+import com.welcomeToJeju.wtj.domain.PlacePhoto;
 
 public interface PlacePhotoDao {
 
   void insert(HashMap<String,Object> param) throws Exception;
   void delete(String id) throws Exception;
+
+  List<PlacePhoto> findAllByPlaceId(String id) throws Exception;
+
 
 }
