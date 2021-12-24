b5049af11ca29c0f6d4f22d7c01c99de78f9ad7f
leeseoeun <seoeun.shuen@gmail.com>
2021-12-24 22:25:53 +0900

    1224 MenuGroup 수정: 13-d 리팩토링

--- app/src/main/java/com/welcomeToJeJu/menu/MenuGroup.java
+++ app/src/main/java/com/welcomeToJeJu/menu/MenuGroup.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Stack;
 import com.welcomeToJeJu.util.Prompt;
-//import com.welcomeToJeJu.wtj.handler.AuthLoginHandler;
+import com.welcomeToJeJu.wtj.handler.AuthLoginHandler;
 
 // 역할
 // - 다른 메뉴를 포함하는 컨테이너 역할을 수행한다.
@@ -21,9 +21,11 @@ public class MenuGroup extends Menu {
   public MenuGroup(String title) {
     super(title);
   }
+
   public MenuGroup(String title, int accessScope) {
-    super(title,accessScope);
+    super(title, accessScope);
   }
+
   public MenuGroup(String title, boolean disablePrevMenu) {
     super(title);
     this.disablePrevMenu = disablePrevMenu;
@@ -49,49 +51,35 @@ public class MenuGroup extends Menu {
     breadCrumb.push(this);
 
     while (true) {
-      System.out.printf("\n[%s]\n", getTitleMenus());
-
-      List<Menu> menuList = new ArrayList<>();
-
-      //      for(Menu menu : childs) {
-      //        if((AuthLoginHandler.getUserAccessLevel() & menu.accessScope) > 0) {
-      //          menuList.add(menu);
-      //        }
-      //      }
-
-      for (int i = 0; i < menuList.size(); i++) {
-        System.out.printf("%d. %s\n", i + 1, menuList.get(i).title);
-      }
-
-      if (!disablePrevMenu) {
-        System.out.printf("0. %s\n", this.prevMenuTitle);
-      }
+      printBreadCrumbMenuTitle();
+      List<Menu> menuList = getMenuList();
+      printMenuList(menuList);
 
       try {
-        int menuNo = Prompt.inputInt("선택> ");
+        int menuNo = Prompt.inputInt("선택 > ");
+
+        if (menuNo < 0 || menuNo > childs.size()) {
+          System.out.println("무효한 메뉴 번호!");
+          continue;
+        }
 
         if (menuNo == 0 && !disablePrevMenu) {
           breadCrumb.pop();
           return;
         }
 
-        if (menuNo < 0 || menuNo > childs.size()) {
-          System.out.println("무효한 메뉴 번호입니다.");
-          continue;
-        }
-
         menuList.get(menuNo - 1).execute();
 
       } catch (Exception e) {
         System.out.println("------------------------------------------");
-        System.out.printf("오류 발생: %s\n", e.getClass().getName());
+        System.out.printf("오류 발생! %s\n", e.getClass().getName());
         System.out.println("------------------------------------------");
       }
 
     }
   }
 
-  public String getTitleMenus() {
+  public String getBreadCrumb() {
     String path = "";
 
     for(Menu menu : breadCrumb) {
@@ -103,6 +91,32 @@ public class MenuGroup extends Menu {
     }
 
     return path;
+  }
+
+  private void printBreadCrumbMenuTitle() {
+    System.out.printf("\n[%s]\n", getBreadCrumb());
+  }
+
+  private List<Menu> getMenuList() {
+    List<Menu> menuList = new ArrayList<>();
+
+    for(Menu menu : childs) {
+      if((menu.accessScope & AuthLoginHandler.getUserAccessLevel()) > 0) {
+        menuList.add(menu);
+      }
+    }
+
+    return menuList;
+  }
+
+  private void printMenuList(List<Menu> menuList) {
+    for (int i = 0; i < menuList.size(); i++) {
+      System.out.printf("%d. %s\n", i + 1, menuList.get(i).title);
+    }
+
+    if (!disablePrevMenu) {
+      System.out.printf("0. %s\n", this.prevMenuTitle);
+    }
   }
 
 
