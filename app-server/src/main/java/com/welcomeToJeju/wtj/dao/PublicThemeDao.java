2a3b41e6f97a6655c2db1b7270ab9042cbba66ed
leeseoeun <seoeun.shuen@gmail.com>
2021-12-28 20:25:45 +0900

    PublicThemeDao, ShareThemeDao 수정: 좋아하는 테마 관련 추가

--- app-server/src/main/java/com/welcomeToJeju/wtj/dao/PublicThemeDao.java
+++ app-server/src/main/java/com/welcomeToJeju/wtj/dao/PublicThemeDao.java
@@ -24,6 +24,13 @@ public interface PublicThemeDao {
 
   List<Theme> findAllPublicThemeByPlaceId(String placeId) throws Exception;
 
+  // 좋아하는 테마
+  void insertLikedTheme(int themeNo, int userNo) throws Exception;
+  void deleteLikedTheme(int themeNo, int userNo) throws Exception;
+  void deleteAllLikedThemeByThemeNo(int themeNo) throws Exception;
+  void deleteAllLikedThemeByUserNo(int userNo) throws Exception;
+  List<Theme> findAllLikedTheme(int userNo) throws Exception;
+
   // 테마 검색
   Theme findByNo(int no) throws Exception;
   Theme findByTitle(String title) throws Exception;
